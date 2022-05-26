6464bc3977c55d6ee70211d993fd60006389e6c6
Goodness Igunma <Goodnessigunma1@gmail.com>
2022-05-26 10:11:17 +0100

    Added Cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Cards from './components/Cards';
 import './style.css';
 
+
 export default function App() {
+
+  // <Hero />
   return(
     <div>
       <Navbar />
+      <Cards />
     </div>
   )
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+import groupImage from "../images/group-image.png";
+
+
+export default function Hero(){
+    return(
+        <section className="hero">
+            <img src={groupImage} alt="groupImage" className="hero--photo" />
+            <h1 className="hero--header">Online Experience</h1>
+            <p className="hero-text">Join unique interative activities lead by
+            one-of-a-kind hosts-all without leaving home.</p>
+        </section>
+    )
+}
